fa10728d0b22fa65595a5e4082ac8b1744c517f2
thaidd7b <thaidd7b@gmail.com>
2021-11-08 22:48:53 +0700

    sua upload, delete

--- api/test/models/test.js
+++ api/test/models/test.js
@@ -12,8 +12,7 @@ module.exports = {
    */
   lifecycles: {
     async afterCreate(result, data) {
-      console.log(15,data);
-      if(data.slug){
+      if (data.slug) {
         const dir = process.env.SERVER_UPLOAD_FOLDER;
         const folder = path.join(dir, "/" + data.slug);
 
@@ -23,55 +22,18 @@ module.exports = {
         if (!fs.existsSync(folder)) {
           fs.mkdirSync(folder);
         }
-
-        const listFile = fs.readdirSync(process.env.FRONTEND_UPLOAD_FOLDER);
-        const listImg  = data.file.filter((ele,index)=>{
-          return ele.url.match(/\.(jpe?g|png|webp|svg)$/)
-        })
-        listImg.map((ele)=>{
-          const name = path.basename(ele.url)
-          const oldName = path.join(process.env.SERVER_UPLOAD_PUBLIC, name)
-          fs.readFile(oldName, function (err, data) {
+        data.file.map((ele) => {
+          const name = path.basename(ele.url);
+          const writeLink = path.join(folder, name);
+          const oldname = ele.url.match(/\.(jpe?g|png|webp|svg)$/)
+            ? path.join(process.env.SERVER_UPLOAD_PUBLIC, name)
+            : ele.url;
+          fs.rename(oldname, writeLink, (err) => {
             if (err) {
               console.log(err);
-            } else {
-              const writeLink = path.join(folder, name);
-              fs.writeFile(writeLink, data, function (err) {
-                if (err) {
-                  console.log(err);
-                } else {
-                  fs.unlink(oldName, function (err) {
-                    if (err) {
-                      console.log(err);
-                    }
-                  });
-                }
-              });
             }
           });
-        })
-
-        for (let i = 0; i < listFile.length; i++) {
-          const link = path.join(process.env.FRONTEND_UPLOAD_FOLDER, listFile[i]);
-          fs.readFile(link, function (err, data) {
-            if (err) {
-              console.log(err);
-            } else {
-              const writeLink = path.join(folder, `/${listFile[i]}`);
-              fs.writeFile(writeLink, data, function (err) {
-                if (err) {
-                  console.log(err);
-                } else {
-                  fs.unlink(link, function (err) {
-                    if (err) {
-                      console.log(err);
-                    }
-                  });
-                }
-              });
-            }
-          });
-        }
+        });
       }
     },
   },
--- providers/strapi-provider-upload-local-file/index.js
+++ providers/strapi-provider-upload-local-file/index.js
@@ -1,43 +1,48 @@
-'use strict';
+"use strict";
 
 /**
  * Module dependencies
  */
 
 // Public node modules.
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 /* eslint-disable no-unused-vars */
 module.exports = {
-  provider: 'strapi-provider-upload-local-path',
-  name: 'Local server',
+  provider: "strapi-provider-upload-local-path",
+  name: "Local server",
   init: (config) => {
     return {
       upload: (file) => {
-        const fileName = file.name.split('.')[0]
+        const fileName = file.name.split(".")[0];
         return new Promise((resolve, reject) => {
           // write file in configured folder
-          fs.writeFile(path.join(config.path , `/${fileName}${file.ext}`), file.buffer, (err) => {
-          if (err) {
-              return reject(err);
-            }
-            if(file.name.match(/\.(jpe?g|png|webp|svg)$/)){
-              file.url = path.join('/uploads' , `/${fileName}${file.ext}`);
-            }else{
-              file.url = path.join(config.path , `/${fileName}${file.ext}`);
-            }
+          fs.writeFile(
+            path.join(config.path, `/${fileName}${file.ext}`),
+            file.buffer,
+            (err) => {
+              if (err) {
+                return reject(err);
+              }
+              if (file.name.match(/\.(jpe?g|png|webp|svg)$/)) {
+                file.url = path.join("/uploads", `/${fileName}${file.ext}`);
+              } else {
+                file.url = path.join(config.path, `/${fileName}${file.ext}`);
+              }
 
-            resolve();
-          });
+              resolve();
+            }
+          );
         });
       },
       delete: (file) => {
         return new Promise((resolve, reject) => {
-          const fileName = file.name.split('.')[0]
-          const filePath = path.join(config.path , `/${fileName}${file.ext}`);
-
+          const fileName = file.name.split(".")[0];
+          console.log(41, file);
+          const filePath = path.join(config.path, `/${fileName}${file.ext}`);
+          console.log(43, filePath);
           if (!fs.existsSync(filePath)) {
-            return resolve('File doesn\'t exist');
+            return resolve("File doesn't exist");
           }
 
           // remove file from public/assets folder
@@ -49,7 +54,7 @@ module.exports = {
             resolve();
           });
         });
-      }
+      },
     };
-  }
+  },
 };
